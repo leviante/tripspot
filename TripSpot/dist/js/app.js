6402042199f88457e7b11cd9d1fcea025f00bb55
Berk Açıkgöz <acikgozberk96@gmail.com>
2019-06-29 22:17:39 +0300

    Finished smooth scrolling with JS, box shadows + popup + responsive navbar left

--- TripSpot/dist/js/app.js
+++ TripSpot/dist/js/app.js
@@ -1,7 +1,10 @@
 //Getting DOM elements
 const header = document.querySelector("#main-nav");
+const navLinks = document.querySelectorAll(".link-item");
 const cardToggle = document.querySelectorAll(".card-link");
 const card = document.querySelector(".card");
+const scrollUp = document.querySelector(".scroll-up");
+const navElementHeights = [];
 
 //Resizing overlay added to elements when resizing the window
 window.addEventListener("resize", () => {
@@ -10,16 +13,27 @@ window.addEventListener("resize", () => {
 });
 
 //Black background in header upon scrolling
-window.addEventListener("scroll", setHeaderBackground);
+window.addEventListener("scroll", () => {
+  setHeaderBackground();
+  setNavActiveClass();
+  //console.log(window.scrollY); gets the scroll pos
+});
+
+//Smooth scrolling to section when clicked navbar links
+navLinks.forEach(navLink => navLink.addEventListener("click", scrollToSection));
 
 //Card slide upon clicking one of cardToggle
-//Define count for card switch links
-let count = 0;
 cardToggle.forEach(cardLink => cardLink.addEventListener("click", toggleCard));
 
+//get scroll amount for footer scroll up element
+scrollUp.addEventListener("click", scrollTop);
+
+//--------------------- THESE WILL RUN WHEN PAGE LOADS ---------------------
+calculateHeight();
+console.log(navElementHeights);
 setOverlay("landing-overlay");
 setOverlay("experience-overlay");
-// FUNCTIONS
+// ----------------------------------------------------------- FUNCTIONS
 
 function setOverlay(str) {
   const classPrefix = str.split("-")[0];
@@ -35,7 +49,7 @@ function setOverlay(str) {
   const overlay = document.querySelector(`.${str}`);
   overlay.style.height = `${eleHeight}px`;
 }
-
+// ------------------------------------------------------------------ //
 function setHeaderBackground() {
   //add header background if window is scrolled
   if (window.scrollY != 0) {
@@ -44,7 +58,7 @@ function setHeaderBackground() {
     header.classList.remove("blk-background");
   }
 }
-
+// ------------------------------------------------------------------ //
 function toggleCard(e) {
   e.preventDefault();
   const cardShowcase = e.target;
@@ -52,37 +66,36 @@ function toggleCard(e) {
   //gets the left offset number
   let offsetStr = window.getComputedStyle(card).left;
   let offsetValue = parseInt(offsetStr.split("px")[0]);
-  console.log(cardToggle[0]);
 
   switch (offsetValue) {
     case 0:
-      if (e.target == cardToggle[0]) {
+      if (cardShowcase == cardToggle[0]) {
         card.style.left = "349px";
         cardToggle[1].classList.remove("active");
         cardToggle[0].classList.add("active");
-      } else if (e.target == cardToggle[2]) {
+      } else if (cardShowcase == cardToggle[2]) {
         card.style.left = "-349px";
         cardToggle[1].classList.remove("active");
         cardToggle[2].classList.add("active");
       }
       break;
     case -349:
-      if (e.target == cardToggle[1]) {
+      if (cardShowcase == cardToggle[1]) {
         card.style.left = "0px";
         cardToggle[1].classList.add("active");
         cardToggle[2].classList.remove("active");
-      } else if (e.target == cardToggle[0]) {
+      } else if (cardShowcase == cardToggle[0]) {
         card.style.left = "349px";
         cardToggle[2].classList.remove("active");
         cardToggle[0].classList.add("active");
       }
       break;
     case 349:
-      if (e.target == cardToggle[1]) {
+      if (cardShowcase == cardToggle[1]) {
         card.style.left = "0px";
         cardToggle[1].classList.add("active");
         cardToggle[0].classList.remove("active");
-      } else if (e.target == cardToggle[2]) {
+      } else if (cardShowcase == cardToggle[2]) {
         card.style.left = "-349px";
         cardToggle[0].classList.remove("active");
         cardToggle[2].classList.add("active");
@@ -92,5 +105,72 @@ function toggleCard(e) {
       break;
   }
 }
+// ------------------------------------------------------------------ //
+function scrollTop(e) {
+  e.preventDefault();
+  window.scroll({
+    top: 0,
+    left: 0,
+    behavior: "smooth"
+  });
+}
+// ------------------------------------------------------------------ //
+function scrollToSection(e) {
+  e.preventDefault();
 
-console.log(cardToggle);
+  const targetElementId = e.currentTarget.getAttribute("href");
+  const targetElement = document.querySelector(`${targetElementId}`);
+
+  //get target element height from top
+  const elementRect = targetElement.getBoundingClientRect();
+  const elementHeight = elementRect.top;
+
+  //animate scroll
+  window.scrollBy({
+    top: elementHeight + 20, //20 added to handle margin differences
+    left: 0,
+    behavior: "smooth"
+  });
+}
+// ------------------------------------------------------------------ //
+function calculateHeight() {
+  window.scrollTo(0, 0);
+
+  navLinks.forEach(navLink => {
+    const targetElementId = navLink.getAttribute("href");
+    const targetElement = document.querySelector(`${targetElementId}`);
+
+    //get target element height (from viewport)
+    const elementRect = targetElement.getBoundingClientRect();
+    const eleViewportHeight = elementRect.top;
+
+    //get target element height (it's total height)
+    const elementHeight = targetElement.offsetHeight;
+
+    const element = {
+      grab: navLink,
+      viewportHeight: eleViewportHeight,
+      eleHeight: elementHeight
+    };
+
+    navElementHeights.push(element);
+  });
+}
+// ------------------------------------------------------------------ //
+function setNavActiveClass() {
+  const winHeight = window.scrollY;
+
+  navElementHeights.forEach(navElement => {
+    const scrollHeight = navElement.viewportHeight;
+    const totalHeight = scrollHeight + navElement.eleHeight;
+
+    if (winHeight >= scrollHeight && winHeight <= totalHeight) {
+      navElement.grab.classList.add("active");
+    } else if (winHeight > totalHeight) {
+      navElement.grab.classList.remove("active");
+    } else if (winHeight < scrollHeight) {
+      navElement.grab.classList.remove("active");
+    }
+  });
+}
+// ------------------------------------------------------------------ //
