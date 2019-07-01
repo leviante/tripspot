8ca61fa682e656d44c45029982173c875fb5a665
Berk Açıkgöz <acikgozberk96@gmail.com>
2019-07-01 23:33:16 +0300

    Finished initial project

--- dist/js/app.js
+++ dist/js/app.js
@@ -223,7 +223,7 @@ function showPopup(e) {
   });
 }
 
-// ----------------------------------------------------------------- //
+// -------------------------------------------------------------- //
 
 function exitPopup() {
   //remove class from popup to make it invisible again
